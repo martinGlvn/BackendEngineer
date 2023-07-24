c74e8a97cf192996c4e0fefc13efc2a93cdcb020
martinGlvn <lkn.galvan.martin@gmail.com>
2023-07-24 14:19:01 -0300

    npm

--- FaztNode/07npm/npm.js
+++ FaztNode/07npm/npm.js
@@ -1,1 +1,7 @@
-console.log("npm");
+// paquete de colores para nuestro codigo en consola
+const colors = require("colors");
+
+console.log("hi".bgGreen);
+
+// modulos requeridos => npm install "..."
+// modulos desarrollo => npm install "..." -D
