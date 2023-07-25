f0816a11f58a62a3f0b9992e80a01414b39760f3
martinGlvn <lkn.galvan.martin@gmail.com>
2023-07-25 19:02:36 -0300

    stats_fs

---
+++ MiduNode/03NativeModules/1_Module_FS/0_StatsFS/index.js
@@ -0,0 +1,11 @@
+// Module FileSystem =>
+const fs = require("node:fs");
+
+const stats = fs.statSync("./index.js");
+
+console.log(
+  stats.isFile(),
+  stats.isDirectory(),
+  stats.isSymbolicLink(),
+  stats.size
+);
