2bc67c6097b8e5c9eea428a1adf87fefa8cbd18c
martinGlvn <lkn.galvan.martin@gmail.com>
2023-07-28 19:23:14 -0300

    sv with frame express

---
+++ 02Express/1_express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile("./static/index.html", {
+    root: __dirname,
+  });
+});
+
+app.listen(3000);
+console.log("sv on port 3000");
