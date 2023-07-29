d14871f144529c186e92f788c04f6ee05259ff12
martinGlvn <lkn.galvan.martin@gmail.com>
2023-07-29 14:53:43 -0300

    express

--- 02Express/7_queries.js
+++ 02Express/7_queries.js
@@ -1,25 +1,16 @@
 const express = require("express");
-
 //
 const app = express();
 
-//
-app.get("/hello/:user", (req, res) => {
-  console.log(req.params.user);
-  res.send(`hi ${req.params.user}`);
-});
-//
-app.get("/add/:x/:y", (req, res) => {
-  const { x, y } = req.params;
-  res.send(`sum of x + y is ${parseInt(x) + parseInt(y)}`);
-});
-//
-app.get("/users/:username/photo", (req, res) => {
-  if (req.params.username === "martin") {
-    return res.sendFile("./me2.jpg", { root: __dirname });
+//Enviamos informacion en la url =>
+app.get("/search", (req, res) => {
+  if (req.query.q === "js") {
+    res.send("books");
+  } else {
+    res.send("page");
   }
-  res.send("not found");
 });
+//
 
 // cfg sv
 app.listen(3000);
