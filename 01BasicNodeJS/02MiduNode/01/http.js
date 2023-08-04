e8bcb4afb1def08b5fb1f961615fb624c9bde3bd
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-03 22:27:40 -0300

    routes sv

--- 01BasicNodeJS/02MiduNode/01/http.js
+++ 01BasicNodeJS/02MiduNode/01/http.js
@@ -4,10 +4,22 @@ const http = require("http");
 const desiredPort = 1234;
 
 // sv
-const server = http.createServer((req, res) => {
-  console.log("req received", req.url);
-  res.end("hi");
-});
+const processRequest = (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("<h1>Welcome to my Website</h1>");
+  } else if (req.url === "/contact") {
+    res.statusCode = 200;
+    res.end("<h1>Contact</h1>");
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>404</h1>");
+  }
+};
+
+//
+const server = http.createServer(processRequest);
 
 // listen sv
 server.listen(desiredPort, () => {
