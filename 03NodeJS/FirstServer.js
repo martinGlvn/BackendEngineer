085e0207c2f0d02d277cd76c5887d79187fca838
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-05 10:59:59 -0300

    firstServerNodeJS

--- 03NodeJS/FirstServer.js
+++ 03NodeJS/FirstServer.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/html");
   res.end("<h1>Welcome to my website</h1>");
 });
 
