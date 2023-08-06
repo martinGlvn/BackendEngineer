2c15c6bb018afd7e9034f35a6f1431a3ac061f3a
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-06 20:56:40 -0300

    .env cfgs

--- 04ApiRest/01/app.js
+++ 04ApiRest/01/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const { PORT } = require("./src/config/config");
 const app = express();
-const PORT = 3000;
+const PORT_APP = PORT || 5000;
 
 // first method
 app.get("/", (req, res) => {
   res.send("Welcome my website");
 });
 
-app.listen(PORT, () => {
-  console.log(`website listening on port: ${PORT}`);
+app.listen(PORT_APP, () => {
+  console.log(
+    `website running in enviroment ${process.env.NODE_ENV} on port: ${PORT}`
+  );
 });
