66faa8c336846f9ce82b7b77b400b44190a77921
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-06 12:35:57 -0300

    ValidateMovies

--- 01FundamentalNodeJS/02MiduNode/02/app.js
+++ 01FundamentalNodeJS/02MiduNode/02/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const crypto = require("crypto");
+const z = require("zod"); //validate data types
 const movies = require("./movies.json");
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 app.disable("x-powered-by");
 
 // Todos los recursos que sean MOVIES se identifican con /movies
 app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLocaleLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
   res.json(movies);
 });
 
@@ -15,6 +25,19 @@ app.get("/movies/:id", (req, res) => {
   const movie = movies.find((movie) => movie.id === id);
   if (movie) return res.json(movie);
   res.status(404).json({ message: "movie not found" });
+});
+
+//
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+  if (result.error) {
+    return res.status(400).json({ error: result.error.message });
+  }
+  const newMovie = {
+    id: crypto.randomUUID(), //uuid => universal unique identifier
+    ...result.data,
+  };
+  movies.push(newMovie);
 });
 
 app.listen(PORT, (req, res) => {
