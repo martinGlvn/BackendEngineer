8dfeeb3c8102c5e9482e6250c69d9463c4813a5c
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-07 20:19:24 -0300

    firstServerExpress

---
+++ 03NodeJS/01/01express.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+//GET-POST-PUT-PATCH-DELETE =>
+app.use();
+
+app.listen(PORT, () => {
+  console.log(`sv listen on port:${PORT}`);
+});
