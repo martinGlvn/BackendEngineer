166eef97dce260e861dbea506250208eb47a5018
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-07 20:19:06 -0300

    firstServerNodeJS

--- 03NodeJS/01/00http.js
+++ 03NodeJS/01/00http.js
@@ -1,1 +1,13 @@
-console.log();
+// HyperTextTransferProtocol =>
+const http = require("http");
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.end("response");
+});
+
+// GET - POST - PUT - DELETE =>
+
+server.listen(port, () => {
+  console.log("🔋 on 🔋");
+});
