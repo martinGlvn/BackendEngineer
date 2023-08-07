5a425af4338b38b2f915fee223ed5a87d87a766f
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-07 11:05:10 -0300

    Middlewares & Cors

--- 04ApiRest/01/src/app.js
+++ 04ApiRest/01/src/app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import { API_VERSION, PORT } from "./config/config.js";
+import cookieParse from "cookie-parser";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import displayRoutes from "express-routemap";
 
 //
 const app = express();
 const PORT_APP = PORT || 5000;
 const API_PREFIX = "api";
+
+// globals middlewares
+app.use(cookieParse());
+app.use(hpp());
+app.use(helmet());
+app.use(cors());
 
 // first method
 app.use(`/${API_PREFIX}/${API_VERSION}/alive`, (req, res) => {
@@ -12,6 +23,7 @@ app.use(`/${API_PREFIX}/${API_VERSION}/alive`, (req, res) => {
 });
 
 app.listen(PORT_APP, () => {
+  displayRoutes(app);
   console.log(
     `website running in enviroment ${process.env.NODE_ENV} on port: ${PORT}`
   );
