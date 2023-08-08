d5d000d4f775c032d56b8a70cbbf5f4e569df5fb
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-07 22:20:45 -0300

    readFilesExpress

--- 03NodeJS/01/02staticFile.js
+++ 03NodeJS/01/02staticFile.js
@@ -1,1 +1,15 @@
-console.log();
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+//GET =>
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+app.get("/martin", (req, res) => {
+  res.send("welcome");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀sv listening on port:${PORT}🚀`);
+});
