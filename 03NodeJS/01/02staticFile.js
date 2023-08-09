4155770ec6b06cee0ecf113ee4fafffbd9a24cb9
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-08 21:28:29 -0300

    ReqBody

--- 03NodeJS/01/02staticFile.js
+++ 03NodeJS/01/02staticFile.js
@@ -4,13 +4,21 @@ const PORT = 5000;
 
 // middleware =>
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // leer req.body
 
-// GET =>
-app.get("/formulario", (req, res) => {
-  res.send("formulario enviado " + req.query.nombre);
-  console.log(req.query);
+// GET => info viaja por url
+// app.get("/formulario", (req, res) => {
+//   res.send("formulario enviado " + req.query.nombre);
+//   console.log(req.query);
+// });
+
+// POST => info viaja por body
+app.post("/formulario", (req, res) => {
+  res.send("formulario enviado " + req.body.nombre);
+  console.log(req.body.nombre);
 });
 
+//
 app.listen(PORT, () => {
   console.log(`🚀sv listening on port:${PORT}🚀`);
 });
