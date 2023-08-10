0dc5b172d369b79fd6893d299c96a6f29b166d4e
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-10 17:14:10 -0300

    CreateServer

--- 01FundamentalNodeJS/03Platzi/01ApiRest/index.js
+++ 01FundamentalNodeJS/03Platzi/01ApiRest/index.js
@@ -1,1 +1,13 @@
-console.log('MyApp');
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// methods => get
+app.get('/', (req, res) => {
+  res.send('sv in express');
+});
+
+// App listen port
+app.listen(PORT, () => {
+  console.log('running sv on port 5000');
+});
