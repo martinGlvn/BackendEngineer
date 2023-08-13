79b24e2ff470de4c0d3651567cc9c348829af5fb
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-13 09:54:02 -0300

    Timers

--- 00FundamentalKnowledge/0_AsyncAwait/00_Timers.js
+++ 00FundamentalKnowledge/0_AsyncAwait/00_Timers.js
@@ -9,3 +9,19 @@ setTimeout(() => {
 setInterval(() => {
   console.log("async");
 }, 1000);
+
+//
+const getUserById = (id, callback) => {
+  const user = {
+    id, //10
+    name: "martin",
+  };
+  setTimeout(() => {
+    callback(user);
+  }, 1500);
+};
+
+getUserById(10, (user) => {
+  console.log(user.id);
+  console.log(user.name);
+});
