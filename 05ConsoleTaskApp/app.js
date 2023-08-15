9e166cc2fca0cba09107802318fa4b3a198ba054
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-14 21:32:25 -0300

    ConsoleTaskApp

--- 05ConsoleTaskApp/app.js
+++ 05ConsoleTaskApp/app.js
@@ -1,8 +1,11 @@
 const colors = require("colors");
 console.log(console.clear);
 
-//
+//Recibir input =>
 const main = async () => {
   console.log("hi");
+  colors.america(console.log("hi"));
 };
 main();
+
+//
