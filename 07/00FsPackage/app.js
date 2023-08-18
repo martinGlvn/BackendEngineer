b1b6a11c5ce01b7b6d4c59a5db309f804adfc9f3
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-18 17:27:15 -0300

    ModuleFS

---
+++ 07/00FsPackage/app.js
@@ -0,0 +1,16 @@
+// packg fs=>
+const fs = require("fs");
+
+// leer contenido de archivo => "Sincrono"
+const data = fs.readFileSync("./data.txt", "utf-8");
+console.log(data);
+
+// Crear archivo => "sincrono"
+const newData = `Contenido nuevo del archivo. Este contenido = "${data}" viene de "data.txt" creado en ${Date.now()}`;
+fs.writeFileSync("./newFile.txt", newData);
+
+//leer contenido de archivo => "Asincrono"
+fs.readFile("./data.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
