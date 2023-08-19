47c33012cebee788646d6aa10ad909a2bd533fc2
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-19 16:45:11 -0300

    InitApiRest

--- 08ApiRest/index.js
+++ 08ApiRest/index.js
@@ -1,8 +1,19 @@
-const express = require("express");
+import "dotenv/config";
+import "./database/connectdb.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+
+//
 const app = express();
+
+const PORT = 5000;
 
 app.get("/", (req, res) => {
   console.log("hi");
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log("");
+});
