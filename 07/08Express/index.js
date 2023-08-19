ef49f1afbab77ae79829f0f4df5b9a86dd7792ab
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-19 12:25:29 -0300

    Express

--- 07/08Express/index.js
+++ 07/08Express/index.js
@@ -1,1 +1,17 @@
-console.log("");
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello", data: [] });
+});
+app.post("/", (req, res) => {
+  res.status(200).json({ message: "Hello", data: [] });
+});
+app.get("/home", (req, res) => {
+  res.status(200).json({ message: "Hello", data: [] });
+});
+
+app.listen(PORT, () => {
+  console.log(`sv running on port ${PORT}`);
+});
