4c46c0c5a7507bbdb80ca8831bf096af0661621e
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-20 13:03:01 -0300

    express validator

---
+++ 08ApiRest/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+
+export const register = (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.json({ ok: true });
+};
+
+export const login = (req, res) => {
+  res.json({ ok: true });
+};
--- 08ApiRest/routes/auth.route.js
+++ 08ApiRest/routes/auth.route.js
@@ -1,10 +1,30 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
+import { body } from "express-validator";
 const router = express.Router();
 
+// register =>
+router.post(
+  "/register",
+  [
+    // email
+    body("email", "🔥 Incorrect format email 🔥")
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    // lenght pass
+    body("password", "🔥 Min 6 characters 🔥").trim().isLength({ min: 6 }),
+    // math pass & repass
+    body("password", "🔥 Incorrect format pass 🔥").custom((value, { req }) => {
+      if (value !== req.body.repassword) {
+        throw new Error("🔥 Password no math 🔥");
+      }
+      return value;
+    }),
+  ],
+  register
+);
 // login =>
 router.post("/login", login);
-// register =>
-router.post("/register", register);
 
 export default router;
