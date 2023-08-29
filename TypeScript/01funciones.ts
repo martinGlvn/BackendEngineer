3cc7e5cfa33f21196af1785bff059216be0b59ad
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-29 18:59:58 -0300

    functions

---
+++ TypeScript/01funciones.ts
@@ -0,0 +1,20 @@
+// inferencia en functions =>
+
+function saludar(name: string) {
+  console.log(`hi ${name}`);
+}
+
+saludar("hi"); // ✅
+// saludar(5);  ❌
+
+// arrow fucntions =>
+const sumar = (a: number, b: number): number => {
+  return a + b;
+};
+
+// never => funciones que no devuelven nada y no se termina de ejecutar...
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+// void => no devuelve nada pero se termina de ejecutar...
