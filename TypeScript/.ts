bab7fc9869a10392f93b93f27fe3064c0210fa66
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-29 14:40:45 -0300

    ts

--- TypeScript/.ts
+++ TypeScript/.ts
@@ -1,1 +1,5 @@
-//
+// Inferir => Typescript sin la necesidad de especificar el tipo de dato
+// comprende que "tipo" estamos utilizando.
+
+const nombre1 = "martin"; //
+const nombre2: string = "martin"; //
