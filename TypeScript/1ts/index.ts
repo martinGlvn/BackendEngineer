a060d8a31139310de5eede6c9ce6ed4e520b5ae8
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-31 22:30:43 -0300

    variables

---
+++ TypeScript/1ts/index.ts
@@ -0,0 +1,10 @@
+// ts no comprende el tipo de dato y lo autocompleta con "ANY"
+let myStringVar;
+let myStringVar2 = "1"; // string
+let myIntVar3 = 5; // number
+
+// Si establecemos que la variable es "STRING" si o si debemos colocarle un valor
+let string: string = "jeje";
+console.log(string);
+
+//
