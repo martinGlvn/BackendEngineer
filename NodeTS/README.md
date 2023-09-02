8163b448ee085e4ea044f94aa1abd93bc3732007
martinGlvn <lkn.galvan.martin@gmail.com>
2023-09-02 13:42:30 -0300

    NodeJS & TypeScript

---
+++ NodeTS/README.md
@@ -0,0 +1,17 @@
+- class validator => validadores de clases sobre su contenido
+
+- cors =>
+
+- dotenv => variables de entorno
+
+- express =>
+
+- morgan => estado de las rutas
+
+- mysql => bd
+
+- typeorm => orm de sql
+
+- typeorm-naming-strategies typescrip =>
+
+- typescript =>
---
+++ NodeTS/src/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+// init sv
+class ServerBootstrap {
+  public app: express.Application = express();
+  private port: number = 8000;
+
+  // init variables
+  constructor() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.listen();
+  }
+
+  //
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`sv listen on port ${this.port}`);
+    });
+  }
+
+  //
+}
+
+new ServerBootstrap();
