4c5b8f92ef1ae7f95d142dac4878f127ec76a03a
martinGlvn <lkn.galvan.martin@gmail.com>
2023-09-02 20:16:19 -0300

    mvc

--- NodeTS/src/server.ts
+++ NodeTS/src/server.ts
@@ -13,16 +13,16 @@ class ServerBootstrap {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(morgan("dev"));
     this.app.use(cors());
-
-    // routes =>
-    this.app.get("/api/hola", (req, res) => {
-      res.status(400).json({ message: "hello world" });
-    });
-
+    this.app.use("/api", this.routers());
     this.listen();
   }
 
-  //
+  // routers
+  routers(): Array<express.Router> {
+    return [];
+  }
+
+  //listen sv
   public listen() {
     this.app.listen(this.port, () => {
       console.log(`sv listen on port ${this.port}`);
