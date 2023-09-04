8582cb3650c63d17a029c555299657a6eb3db877
martinGlvn <lkn.galvan.martin@gmail.com>
2023-09-03 21:29:50 -0300

    ObjectsLiterals

---
+++ JavaScript/POO/00ObjetosLiretales.js
@@ -0,0 +1,12 @@
+// POO =>
+
+// Objetos Literales =>
+const persona = {
+  name: "a",
+  age: 25,
+  z() {
+    return `programacion`;
+  },
+};
+console.log(persona.z());
+console.log(persona.age);
